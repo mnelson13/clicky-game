2757283e0681eb2c0ea6245edcc549cf17cede8b
MNelson <melissa.nelson13@yahoo.com>
2018-11-17 01:30:33 -0600

    Updates

---
+++ precache-manifest.3edd13b8c0d5cdb2ed69d7f116deb209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d12073176d962c018009eb7176fd5d9",
+    "url": "/clicky-game/static/media/hypnotize.3d120731.png"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "2ed2643d41a2e0de03ea",
+    "url": "/clicky-game/static/js/main.2ed2643d.chunk.js"
+  },
+  {
+    "revision": "e67643045b21357ff2e1",
+    "url": "/clicky-game/static/js/1.e6764304.chunk.js"
+  },
+  {
+    "revision": "2ed2643d41a2e0de03ea",
+    "url": "/clicky-game/static/css/main.582d8642.chunk.css"
+  },
+  {
+    "revision": "e2c47e11344f22573db924fa05a21fa8",
+    "url": "/clicky-game/index.html"
+  }
+];
