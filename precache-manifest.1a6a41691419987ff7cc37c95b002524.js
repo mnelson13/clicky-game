0a2f4385f32ba3b6d4573a0e22c57b12bb71bc69
MNelson <melissa.nelson13@yahoo.com>
2019-12-08 21:29:43 -0600

    Updates

---
+++ precache-manifest.1a6a41691419987ff7cc37c95b002524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d12073176d962c018009eb7176fd5d9",
+    "url": "/clicky-game/static/media/hypnotize.3d120731.png"
+  },
+  {
+    "revision": "c63fa35b3345fec6693eb5a0ccdcd403",
+    "url": "/clicky-game/static/media/banner.c63fa35b.webp"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "96e31377d6b3a0022069",
+    "url": "/clicky-game/static/js/main.96e31377.chunk.js"
+  },
+  {
+    "revision": "1112fe7293a934349f54",
+    "url": "/clicky-game/static/js/1.1112fe72.chunk.js"
+  },
+  {
+    "revision": "96e31377d6b3a0022069",
+    "url": "/clicky-game/static/css/main.b664957b.chunk.css"
+  },
+  {
+    "revision": "c6a62135be358ef4a329eddb6f3c4c64",
+    "url": "/clicky-game/index.html"
+  }
+];
--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Wrapper from './components/Wrapper';
-
-const App = () => <Wrapper/>
-
-export default App;
